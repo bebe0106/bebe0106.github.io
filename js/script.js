afb26b5e375e92754ca71ff94c7192049c7ce8b8
=Benoit Marchadier <marchadier.benoit@gmail.com>
2023-01-08 12:55:59 +0100

    Petits ajustements

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 timeline
-  .to(".blason",0.0001,{"z-index":-1})
-  .to(".bg", 3, { y: -500 })
-  .to(".content",3,{ y: -500 },"-=3")
-  .to(".blason",3,{ y: 0 },"-=3")
-  /*.to(".blason",3,{opacity: 0.5},"-=3")*/
+    .to(".blason", 0.0001, { "z-index": -1 })
+    .to(".bg", 3, { y: -500 })
+    .to(".content", 3, { y: -500 }, "-=3")
+    .to(".blason", 3, { y: 0 }, "-=3")
+    /*.to(".blason",3,{opacity: 0.5},"-=3")*/
 
-  let scene = new ScrollMagic.Scene({
-  triggerElement: "section",
-  duration: "100%",
-  triggerHook: 0,
-})
-  .setTween(timeline)
-  .setPin("section")
-  .addTo(controller);
+let scene = new ScrollMagic.Scene({
+        triggerElement: "section",
+        duration: "100%",
+        triggerHook: 0,
+    })
+    .setTween(timeline)
+    .setPin("section")
+    .addTo(controller);
